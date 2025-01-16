0db0c576fdd30ecd00dce37e7f0acf970c405e27
Ardent <121909206+ardent83@users.noreply.github.com>
2025-01-16 10:49:03 +0330

    create model Subtitle

--- spotify_subtitle/subtitles/models.py
+++ spotify_subtitle/subtitles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subtitle(models.Model):
+    song_id = models.CharField(max_length=255, unique=True)
+    subtitle_text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Subtitle for song ID: {self.song_id}"
