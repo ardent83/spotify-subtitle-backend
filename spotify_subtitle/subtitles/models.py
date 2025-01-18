97143ac8438c71f1621827fdd9c967c8ab7efe8d
Ardent <121909206+ardent83@users.noreply.github.com>
2025-01-18 21:40:21 +0330

    change text to Segment class

--- spotify_subtitle/subtitles/models.py
+++ spotify_subtitle/subtitles/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import F, Q
 
 
 class Subtitle(models.Model):
-    song_id = models.CharField(max_length=255, unique=True)
-    subtitle_text = models.TextField()
+    song_id = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('song_id', 'user')
+
     def __str__(self):
         return f"Subtitle for song ID: {self.song_id}"
+
+
+class Segment(models.Model):
+    subtitle = models.ForeignKey(Subtitle, on_delete=models.CASCADE, related_name='segments')
+    segment_number = models.IntegerField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    text = models.CharField(max_length=512)
+
+    class Meta:
+        ordering = ('subtitle', 'segment_number')
+        constraints = [
+            models.CheckConstraint(
+                check=Q(start_time__lt=F('end_time')),
+                name='start_before_end',
+            )
+        ]
+
+    def __str__(self):
+        return f"Text: {self.text}"
